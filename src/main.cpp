cd3cc8aadcad7a206a6fb7dce2949d2c7dc03c1d
Hé Táo (´^ω^`) <hetao6001@gmail.com>
2024-10-16 12:07:38 +1100

    up

--- src/main.cpp
+++ src/main.cpp
@@ -15,29 +15,134 @@ bool sample(double p)
 {
     return rnd() % 1000000 < p * 1000000;
 }
-struct Model
+template <typename T>
+struct Node
 {
-    struct Node
-    {
-        S1 state1;
-        S2 state2;
-    };
-    double betau, betaa, mu, lambda, theta;
-    vector<Node> nodes;
-    Model(int n, double betau, double betaa, double mu, double lambda, double theta, double initIRate) : betau(betau), betaa(betaa), mu(mu), lambda(lambda), theta(theta)
+    T rate1, rate2;
+};
+template <>
+struct Node<int>
+{
+    S1 state1;
+    S2 state2;
+};
+template <typename T>
+class Model
+{
+public:
+    double betau, betaa, mu, lambda, delta;
+    vector<Node<T>> nodes;
+    Model(int n, double betau, double betaa, double mu, double lambda, double delta, double initIRate, double initARate) : betau(betau), betaa(betaa), mu(mu), lambda(lambda), delta(delta)
     {
         nodes.resize(n);
         for (int i = 0; i < n; ++i)
-        {
-            nodes[i].state1 = i < n * initIRate ? S1::I : S1::S;
-            nodes[i].state2 = S2::U;
-        }
+            nodes[i].rate1 = i < n * initIRate ? 1 : 0;
+        for (int i = 0; i < n; ++i)
+            swap(nodes[i], nodes[rnd() % n]);
+        for (int i = 0; i < n; ++i)
+            nodes[i].rate2 = i < n * initARate ? 1 : 0;
+        for (int i = 0; i < n; ++i)
+            swap(nodes[i], nodes[rnd() % n]);
+    }
+    void next(const Graph &g1, const Graph &g2)
+    {
+        int n = this->nodes.size();
+        vector<Node<T>> nodes = this->nodes;
         for (int i = 0; i < n; ++i)
         {
-            swap(nodes[i], nodes[rnd() % n]);
+            if (nodes[i].rate1 == S1::I)
+            {
+                if (sample(this->mu))
+                    this->nodes[i].state1 = S1::S;
+            }
+            else
+            {
+                for (int j = g1.head[i]; j; j = g1.edge[j].next)
+                {
+                    int b = g1.edge[j].b;
+                    if (nodes[b].state1 == S1::I && sample(nodes[b].state2 == S2::U ? this->betau : this->betaa))
+                    {
+                        this->nodes[i].state1 = S1::I;
+                        break;
+                    }
+                }
+            }
+            if (nodes[i].state2 == S2::A)
+            {
+                if (sample(this->delta))
+                    this->nodes[i].state2 = S2::U;
+            }
+            else
+            {
+                for (int j = g2.head[i]; j; j = g2.edge[j].next)
+                {
+                    int b = g1.edge[j].b;
+                    if (nodes[b].state2 == S2::A && sample(this->lambda))
+                    {
+                        this->nodes[i].state2 = S2::A;
+                        break;
+                    }
+                }
+            }
         }
     }
 };
+template <>
+Model<int>::Model(int n, double betau, double betaa, double mu, double lambda, double delta, double initIRate, double initARate) : betau(betau), betaa(betaa), mu(mu), lambda(lambda), delta(delta)
+{
+    nodes.resize(n);
+    for (int i = 0; i < n; ++i)
+        nodes[i].state1 = i < n * initIRate ? S1::I : S1::S;
+    for (int i = 0; i < n; ++i)
+        swap(nodes[i], nodes[rnd() % n]);
+    for (int i = 0; i < n; ++i)
+        nodes[i].state2 = i < n * initARate ? S2::A : S2::U;
+    for (int i = 0; i < n; ++i)
+        swap(nodes[i], nodes[rnd() % n]);
+}
+template <>
+void Model<int>::next(const Graph &g1, const Graph &g2)
+{
+    int n = this->nodes.size();
+    vector<Node<int>> nodes = this->nodes;
+    for (int i = 0; i < n; ++i)
+    {
+        if (nodes[i].state1 == S1::I)
+        {
+            if (sample(this->mu))
+                this->nodes[i].state1 = S1::S;
+        }
+        else
+        {
+            for (int j = g1.head[i]; j; j = g1.edge[j].next)
+            {
+                int b = g1.edge[j].b;
+                if (nodes[b].state1 == S1::I && sample(nodes[b].state2 == S2::U ? this->betau : this->betaa))
+                {
+                    this->nodes[i].state1 = S1::I;
+                    break;
+                }
+            }
+        }
+        if (nodes[i].state2 == S2::A)
+        {
+            if (sample(this->delta))
+                this->nodes[i].state2 = S2::U;
+        }
+        else
+        {
+            for (int j = g2.head[i]; j; j = g2.edge[j].next)
+            {
+                int b = g1.edge[j].b;
+                if (nodes[b].state2 == S2::A && sample(this->lambda))
+                {
+                    this->nodes[i].state2 = S2::A;
+                    break;
+                }
+            }
+        }
+    }
+}
 int main(int argc, char *argv[])
 {
     int n = 1e4;
@@ -91,53 +196,12 @@ int main(int argc, char *argv[])
     }
     auto [g1, g2] = generate(n, 2, input, input);
     n = g1.n;
-    Model model(n, 0.1, 0.1, 0.1, 0.1, 0.1, 0.001);
+    Model<int> model(n, 0.15, 0.1, 0.1, 0.1, 0.1, 0.001, 0.001);
     int T = 100;
     for (int t = 0; t < T; ++t)
     {
-        vector<Model::Node> nodes = model.nodes;
-        for (int i = 0; i < n; ++i)
-        {
-            if (nodes[i].state1 == S1::I)
-            {
-                if (sample(model.mu))
-                {
-                    model.nodes[i].state1 = S1::S;
-                }
-            }
-            else
-            {
-                for (int j = g1.head[i]; j; j = g1.edge[j].next)
-                {
-                    int b = g1.edge[j].b;
-                    if (nodes[b].state1 == S1::I && sample(nodes[b].state2 == S2::U ? model.betau : model.betaa))
-                    {
-                        model.nodes[i].state1 = S1::I;
-                        break;
-                    }
-                }
-            }
-            if (nodes[i].state2 == S2::A)
-            {
-                if (sample(model.theta))
-                {
-                    model.nodes[i].state2 = S2::U;
-                }
-            }
-            else
-            {
-                for (int j = g2.head[i]; j; j = g2.edge[j].next)
-                {
-                    int b = g1.edge[j].b;
-                    if (nodes[b].state2 == S2::A && sample(model.lambda))
-                    {
-                        model.nodes[i].state2 = S2::A;
-                        break;
-                    }
-                }
-            }
-        }
         int cnt = 0;
+        model.next(g1, g2);
         for (int i = 0; i < n; ++i)
         {
             if (model.nodes[i].state1 == S1::I)
